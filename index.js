d73b56bb487060bbdd24d7317cd8a361b7313abd
ChristianAtmaluhur <2111500002@mahasiswa.atmaluhur.ac.id>
2022-04-23 12:22:29 +0700

    Menambahkan fitur tambahan berupa input panjang character password

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const chars = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\'', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']
 
-const generateBtn = document.querySelector('.generate-btn');
+
 const boxs = document.querySelectorAll('.box')
+const form = document.querySelector('form')
 
-generateBtn.addEventListener('click', () => {
+form.addEventListener('submit', e => {
+    e.preventDefault() 
+    let num = 0
+
+    const input = form.querySelector('input')
+
+    if(input.value == '') {
+        num = 16
+        console.log('ok');
+    } else if (input.value < 5 || input.value > 16) {
+        return false
+    } else {
+        num = input.value
+    }
+
     boxs.forEach(box => {
-        generate(box)
+        generate(box, num)
     });
-});
+})
 
-function generate(box) {
+function generate(box, number) {
     box.textContent = ''
-    for(let i = 0; i < 16; i++) {
+    for(let i = 0; i < number; i++) {
         const num = Math.floor(Math.random() * 94)
         const char = chars[num]
         box.textContent += char
